d620d89344ad7ee38092873dc7f602761e240e4c
Paulius Juknis <paulius.juknis@gmail.com>
2018-06-06 14:06:08 +0300

    a bit of string interpolation

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -8,5 +8,27 @@ import { Component } from '@angular/core';
 })
 
 export class UserComponent {
+  // Properties
+  firstName = 'John';
+  lastName = 'Smith';
+  age = 30;
+  address = {
+    street: '50 Main st',
+    city: 'Boston',
+    state: 'MA'
+  };
 
+  // Methods
+  constructor() {
+    console.log('Hello user...');
+    // this.sayHello();
+  }
+
+  sayHello() {
+    console.log(`Hello ${this.firstName}`);
+  }
+
+  hasBirthday() {
+    this.age += 1;
+  }
 }
